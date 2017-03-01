331545687a5e3ac63939e9be3bcab466397e190f
Christian Salvatore <christian.salvatore@ibfm.cnr.it>
2017-03-01 14:36:19 +0100

    Update bib-publication-list.js

--- publications/src/bib-publication-list.js
+++ publications/src/bib-publication-list.js
@@ -63,7 +63,7 @@ var bibtexify = (function($) {
             var authorsStr = '';
             for (var index = 0; index < authorData.length; index++) {
                 if (index > 0) { authorsStr += ", "; }
-                authorsStr += authorData[index].last;
+                authorsStr += authorData[index].last += authorData[index].first;
             }
             return htmlify(authorsStr);
         },
